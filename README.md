de8fa1d9e685380cac82e1958cc75a51fb976a5e
PUGE <100284685@qq.com>
2018-06-04 17:47:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# notifications-puge
+
+安装
+```
+npm i -save notifications-puge
+或
+yarn add notifications-puge
+```
+
+## 参数
+
+| 参数        | 含义         | 类型  | 是否必须  |
+| ----------- |:-------------:| -----:| -----:|
+|value| 显示的数值 | Number | true |
+|animation| 显示的动画| String | false |
+|color| 汽泡文字颜色| String | false |
+|background| 汽泡背景色| String | false |
+
+## 动画样式
+| 参数        | 动画效果         |
+| ----------- |:-------------:|
+|默认| ![default](http://p5qgrn52w.bkt.clouddn.com/notifications-puge/default.gif) |
+|rubberBand| ![rubberBand](http://p5qgrn52w.bkt.clouddn.com/notifications-puge/rubberBand.gif) |
+|big| ![big](http://p5qgrn52w.bkt.clouddn.com/notifications-puge/big.gif) |
+|swing| ![swing](http://p5qgrn52w.bkt.clouddn.com/notifications-puge/swing.gif) |
+|tada| ![tada](http://p5qgrn52w.bkt.clouddn.com/notifications-puge/tada.gif) |
+|wobble| ![wobble](http://p5qgrn52w.bkt.clouddn.com/notifications-puge/wobble.gif) |
---
+++ board.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="clock">
+    <div class="clock__number-wrapper" :class="{clock__number_wrapper_open: isOpen}">
+      <span class="clock__section clock__section--next">
+        <span class="clock__number js-increment-after">{{num}}</span>
+      </span>
+      <span class="clock__section clock__section--upper">
+        <span class="clock__number js-increment-before">{{oldNum}}</span>
+      </span>
+      <span class="clock__section clock__section--upper-back">
+        <span class="clock__number js-increment-after">{{num}}</span>
+      </span>
+      <span class="clock__section clock__section--lower">
+        <span class="clock__number js-increment-before">{{oldNum}}</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    num: Number
+  },
+  data () {
+    return {
+      oldNum: 0,
+      isOpen: false
+    }
+  },
+  watch: {
+    num (newNum, old) {
+      this.isOpen = true
+      setTimeout(() => {
+        this.oldNum = old
+        this.isOpen = false
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.clock {
+  width: 80px;
+  height: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -40px;
+  margin-left: -50px;
+  font-family: Arial;
+  line-height: 1;
+  perspective: 400px;
+}
+
+.clock__section {
+  display: block;
+  background-color: #333;
+  color: #fff;
+  position: absolute;
+  left: 0;
+  font-size: 100px;
+  width: 80px;
+  height: 49px;
+  overflow: hidden;
+  backface-visibility: hidden;
+}
+
+.clock__section--lower:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 80px;
+  height: 49px;
+  opacity: 0;
+  background-color: rgba(0, 0, 0, 0.75);
+  transition: none;
+}
+.clock__number_wrapper_open .clock__section--lower:after {
+  transition: opacity 0.5s;
+  opacity: 1;
+}
+
+.clock__section--next {
+  top: 0;
+  z-index: 1;
+  transform-origin: 50% 100%;
+}
+
+.clock__section--upper {
+  top: 0;
+  z-index: 1;
+  transform-origin: 50% 100%;
+  transition: none;
+}
+.clock__number_wrapper_open .clock__section--upper {
+  transition: transform 0.5s;
+  transform: rotateX(-180deg);
+}
+
+.clock__section--upper-back {
+  top: 0;
+  z-index: 7;
+  transform-origin: 50% 50px;
+  transition: none;
+  transform: rotateY(180deg) rotateX(-360deg);
+}
+.clock__number_wrapper_open .clock__section--upper-back {
+  transition: transform 0.5s;
+  transform: rotateY(180deg) rotateX(-180deg);
+}
+
+.clock__section--lower {
+  margin-top: -40px;
+  bottom: 0;
+}
+
+.clock__number {
+  position: absolute;
+  text-align: center;
+  width: 80px;
+}
+.clock__section--lower .clock__number {
+  bottom: 0;
+}
+.clock__section--upper-back .clock__number {
+  bottom: 0;
+  transform: rotateZ(180deg) translateY(-52px);
+}
+</style>
